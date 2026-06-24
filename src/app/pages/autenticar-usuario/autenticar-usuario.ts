a55cbed8dcca4a0b28a797f3cca00ad6028437f9
alison <adevpr11@gmail.com>
2026-06-24 10:33:59 -0300

    Remove conteúdo default de app.html e adiciona estrutura inicial de componentes

---
+++ src/app/pages/autenticar-usuario/autenticar-usuario.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-autenticar-usuario',
+  imports: [],
+  templateUrl: './autenticar-usuario.html',
+  styleUrl: './autenticar-usuario.css',
+})
+export class AutenticarUsuario {}
