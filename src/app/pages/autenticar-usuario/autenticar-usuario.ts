fc4cc05e8a1ec12934ca6f0094706ba09abc564c
alison <adevpr11@gmail.com>
2026-06-24 16:27:30 -0300

    Implementa funcionalidade para autenticar usuário

--- src/app/pages/autenticar-usuario/autenticar-usuario.ts
+++ src/app/pages/autenticar-usuario/autenticar-usuario.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-autenticar-usuario',
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './autenticar-usuario.html',
   styleUrl: './autenticar-usuario.css',
 })
-export class AutenticarUsuario {}
+export class AutenticarUsuario {
+
+  private httpClient = inject(HttpClient);
+
+  private URL = "http://localhost:8082/api/v1/usuarios/";
+
+  formAutenticar = new FormGroup({
+    email: new FormControl('', [Validators.required]),
+    senha: new FormControl('', [Validators.required]),
+  });
+
+  autenticar() {
+    this.httpClient.post(this.URL + "autenticar", this.formAutenticar.value)
+    .subscribe({
+      next: (resposta) => {
+        console.log(resposta);
+      },
+      error: (erro) => {
+        console.error(erro);
+      }
+    });
+  }
+}
