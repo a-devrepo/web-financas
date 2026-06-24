fd9a85a3928f1ecdb51a3806a92301b9b9643829
alison <adevpr11@gmail.com>
2026-06-24 11:04:23 -0300

    Adiciona rotas de navegação

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { AutenticarUsuario } from './pages/autenticar-usuario/autenticar-usuario';
+import { Dashboard } from './pages/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'pages/autenticar',
+        component: AutenticarUsuario
+    },
+    {
+        path: 'pages/criar-usuario',
+        component: AutenticarUsuario
+    },
+    {
+        path: 'pages/dashboard',
+        component: Dashboard
+    },
+    {
+        path: '', pathMatch: 'full',
+        redirectTo: 'pages/autenticar'
+    }
+];
