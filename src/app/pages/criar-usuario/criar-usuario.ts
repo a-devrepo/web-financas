b05c3edf159e4b8d976667a9a0dbd301d83fb61e
alison <adevpr11@gmail.com>
2026-06-26 12:59:27 -0300

    Adiciona página de criação de usuários e integração com API

--- src/app/pages/criar-usuario/criar-usuario.ts
+++ src/app/pages/criar-usuario/criar-usuario.ts
@@ -1,9 +1,48 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-criar-usuario',
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './criar-usuario.html',
   styleUrl: './criar-usuario.css',
 })
-export class CriarUsuario {}
+export class CriarUsuario {
+
+  private regexSenha = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9]).{8,}$/
+
+  private httpClient = inject(HttpClient);
+
+  formCriarUsuario = new FormGroup({
+    nome: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    senha: new FormControl('', [Validators.required, Validators.pattern(this.regexSenha)]),
+    senhaConfirmacao: new FormControl('', [Validators.required]),
+    aceiteTermos: new FormControl(false, [Validators.requiredTrue])
+  });
+
+  criarUsuario() {
+
+    const json = {
+      nome: this.formCriarUsuario.value.nome,
+      email: this.formCriarUsuario.value.email,
+      senha: this.formCriarUsuario.value.senha
+    }
+
+    this.httpClient.post('http://localhost:8082/api/v1/usuarios/criar', json)
+      .subscribe({
+        next: (response) => {
+          console.log('Usuário criado com sucesso:', response);
+        },
+        error: (error) => {
+          console.error('Erro ao criar usuário:', error);
+        }
+      })
+  }
+
+}
